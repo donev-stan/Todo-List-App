4cec543ab14547dd81d88e317bf501fcb3a164bf
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-09 17:58:10 +0430

    local storage

--- todo-list/src/app/shared/task.service.ts
+++ todo-list/src/app/shared/task.service.ts
@@ -5,23 +5,7 @@ import { Task } from './task';
   providedIn: 'root',
 })
 export class TaskService {
-  private _tasks: Task[] = JSON?.parse(localStorage?.getItem('tasks')!) || [
-    {
-      id: 1,
-      text: 'Learn Angular Framework',
-      checked: false,
-    },
-    {
-      id: 2,
-      text: 'Learn Angular Styling Library',
-      checked: false,
-    },
-    {
-      id: 3,
-      text: 'Watch Angular Tutorials',
-      checked: true,
-    },
-  ];
+  private _tasks: Task[] = this.retrieveData();
 
   getTasks(): Task[] {
     return this._tasks;
@@ -30,5 +14,27 @@ export class TaskService {
   setTasks(tasks: Task[]): void {
     this._tasks = tasks;
     localStorage.setItem('tasks', JSON.stringify(this._tasks));
+  }
+
+  retrieveData(): Task[] {
+    return (
+      JSON?.parse(localStorage?.getItem('tasks')!) || [
+        {
+          id: 1,
+          text: 'Learn Angular Framework',
+          checked: false,
+        },
+        {
+          id: 2,
+          text: 'Learn Angular Styling Library',
+          checked: false,
+        },
+        {
+          id: 3,
+          text: 'Watch Angular Tutorials',
+          checked: true,
+        },
+      ]
+    );
   }
 }
