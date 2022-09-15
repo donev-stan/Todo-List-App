1f78469ef29d6c753b6db53505c73a63a7c683a3
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-15 12:30:21 +0430

    additional filtering added

--- todo-list/src/app/stats/stats.component.ts
+++ todo-list/src/app/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-stats',
@@ -8,4 +8,10 @@ import { Component, Input } from '@angular/core';
 export class StatsComponent {
   @Input() completedTasksCount: number = 0;
   @Input() ongoingTasksCount: number = 0;
+
+  @Output() filterTasks: EventEmitter<string> = new EventEmitter();
+
+  chnageSeleltedTasks(selector: string): void {
+    this.filterTasks.emit(selector);
+  }
 }
